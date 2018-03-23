f6407475b0f479abd687620226572e23b9b513c9
georgemakrakis <georgemakrakis88@gmail.com>
2018-03-23 18:34:36 +0200

    Initialize commit

---
+++ publishMessage.js
@@ -0,0 +1,69 @@
+let Mam = require('./lib/mam.node.js');
+let IOTA = require('iota.lib.js');
+var fs = require('fs');
+
+// LIVE NODE !
+let iota = new IOTA({provider: `https://testnet140.tangle.works`});
+
+let yourMessage = 'IOTA Masked Authenticated Messaging Send';
+
+
+let seed = fs.readFileSync('s33d.txt', 'utf8');
+
+let mamState = null;
+
+async function fetchStartCount()
+{
+    let trytes = iota.utils.toTrytes('START');
+    let message = Mam.create(mamState, trytes);
+    console.log('The first root:');
+    console.log(message.root);
+    console.log();
+    // Fetch all the messages upward from the first root.
+    return await Mam.fetch(message.root, 'public', null, null);
+    //return await Mam.fetch(message.root, 'private', null, null);
+    //return await Mam.fetch(message.root, 'restricted', null, null);
+}
+
+async function publish(packet)
+{
+    // Create the message.
+    let trytes = iota.utils.toTrytes(JSON.stringify(packet));
+    let message = Mam.create(mamState, trytes);
+    // Set the mam state so we can keep adding messages.
+    mamState = message.state;
+    console.log('Sending message: ', packet);
+    console.log('Root: ', message.root);
+    console.log('Address: ', message.address);
+    console.log();
+    // Attach the message.
+    return await Mam.attach(message.payload, message.address);
+}
+
+// Initiate the mam state with the given seed at index 0.
+mamState = Mam.init(iota, seed, 2, 0);
+
+// Fetch all the messages in the stream.
+fetchStartCount().then(v =>
+{
+    // Log the messages.
+    let startCount = v.messages.length;
+    console.log('Messages already in the stream:');
+    for (let i = 0; i < v.messages.length; i++)
+    {
+        let msg = v.messages[i];
+        console.log(JSON.parse(iota.utils.fromTrytes(msg)));
+    }
+    console.log();
+
+    // To add messages at the end we need to set the startCount for the mam state to the current amount of messages.
+    mamState = Mam.init(iota, seed, 2, startCount);
+
+    let newMessage = Date.now() + ' ' + yourMessage;
+
+    // Now the mam state is set, we can add the message.
+    publish(newMessage);
+}).catch(ex =>
+{
+    console.log(ex);
+});
