b8a11d1d5d903ff6b271cf50a9ed07cbf7519997
georgemakrakis <georgemakrakis88@gmail.com>
2018-06-19 12:34:02 +0300

    Added back the simple publish message file

--- publishMessage.js
+++ publishMessage.js
@@ -2,31 +2,33 @@
 let Mam = require('./lib/mam.node.js');
 let IOTA = require('iota.lib.js');
 let fs = require('fs');
-let sensor = require('node-dht-sensor');
-
-//LIVE NODE !
+// LIVE NODE !
 let iota = new IOTA({ provider: `https://testnet140.tangle.works:443` });
 
-let yourMessage = 'MAM Send from RPi at foss';
+let yourMessage = 'IOTA MAM Thesis send from pc';
 
 //SEED must be 81 chars of A-Z9 //
 let seed = fs.readFileSync('s33d.txt', 'utf8');
-let side_key = fs.readFileSync('side_key.txt', 'utf8');
+let side_key = fs.readFileSync('side_key.txt','utf8');
+
+//This was in case tha a newline character was at the end of file
+//seed = seed.slice(0, -1);
+
+console.log(seed.length);
 
 let mamState = null;
 
-async function fetchStartCount() 
-{
+async function fetchStartCount(){
     let trytes = iota.utils.toTrytes('START');
     let message = Mam.create(mamState, trytes);
     console.log('The first root:');
     console.log(message.root);
     console.log();
     // Fetch all the messages upward from the first root.
-    return await Mam.fetch(message.root, 'restricted', side_key, null);
+    return await Mam.fetch(message.root, 'restricted',side_key, null);
 }
 
-async function publish(packet) 
+async function publish(packet)
 {
     // Create the message.
     let trytes = iota.utils.toTrytes(JSON.stringify(packet));
@@ -44,59 +46,28 @@ async function publish(packet)
 // Initiate the mam state with the given seed at index 0.
 mamState = Mam.init(iota, seed, 2, 0);
 
-sensor.read(22, 4, function (err, temperature, humidity) {
-    if (!err) 
-    {
-        // Fetch all the messages in the stream.
-        fetchStartCount().then(v => {
-            // Log the messages.
-            let startCount = v.messages.length;
-            console.log('Messages already in the stream:');
-            for (let i = 0; i < v.messages.length; i++) {
-                let msg = v.messages[i];
-                console.log(JSON.parse(iota.utils.fromTrytes(msg)));
-            }
-            console.log();
-
-            // To add messages at the end we need to set the startCount for the mam state$
-            mamState = Mam.init(iota, seed, 2, startCount);
-            mamState = Mam.changeMode(mamState, 'restricted', side_key);
-
-            //let newMessage = Date.now() + ' ' + yourMessage;
-
-            let newMessage = Date.now() + ' ' + 'temp: ' + temperature.toFixed(1) + '  C, ' +
-                'humidity: ' + humidity.toFixed(1) + '%';
-
-            // Now the mam state is set, we can add the message.
-            publish(newMessage);
-        }).catch(ex => {
-            console.log(ex);
-        });
+// Fetch all the messages in the stream.
+fetchStartCount().then(v =>
+{
+    // Log the messages.
+    let startCount = v.messages.length;
+    console.log('Messages already in the stream:');
+    for (let i = 0; i < v.messages.length; i++){
+        let msg = v.messages[i];
+        console.log(JSON.parse(iota.utils.fromTrytes(msg)));
     }
-    else 
-    {
-        fetchStartCount().then(v => {
-            // Log the messages.
-            let startCount = v.messages.length;
-            console.log('Messages already in the stream:');
-            for (let i = 0; i < v.messages.length; i++) {
-                let msg = v.messages[i];
-                console.log(JSON.parse(iota.utils.fromTrytes(msg)));
-            }
-            console.log();
+    console.log();
 
-            // To add messages at the end we need to set the startCount for the mam state$
-            mamState = Mam.init(iota, seed, 2, startCount);
-            mamState = Mam.changeMode(mamState, 'restricted', side_key);
+    // To add messages at the end we need to set the startCount for the mam state to the current amount of messages.
+    mamState = Mam.init(iota, seed, 2, startCount);
+    mamState = Mam.changeMode(mamState, 'restricted',side_key);
 
-            //let newMessage = Date.now() + ' ' + yourMessage;
+    let newMessage = Date.now() + ' ' + yourMessage;
 
-            let newMessage = 'No data from sensor, writing dump data: 15 C, humidity: 50';
+    // Now the mam state is set, we can add the message.
+    publish(newMessage);
 
-            // Now the mam state is set, we can add the message.
-            publish(newMessage);
-        }).catch(ex => {
-            console.log(ex);
-        });
-    }
+}).catch(ex =>
+{
+    console.log(ex);
 });
